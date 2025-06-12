bc3ce982da0a12db207251bd5d42e8dbfc15f21b
Artifowl <artichoufowl@gmail.com>
2025-06-12 16:13:26 +0900

    new test.c

--- app/test.c
+++ app/test.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 #include <string.h>
 
-
 int main() {
-    int x;
-    for (int x = 0; x < 10; x++) {
-        int y = y + 1;
-    }
+    int value;
+    printf("Value : %d\n", value);
+
+    char small[10];
+    strcpy(small, "this text is too long for the buffer");
+
+    char input[20];
+    printf("Enter some text: ");
+    gets(input);  
+
+    return 0;
 }
